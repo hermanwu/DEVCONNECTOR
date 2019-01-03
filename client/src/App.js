b035491d8fec9f3affafbe41d21d7cb24e8e201d
Herman Wu <herman@cloudistics.com>
2019-01-02 16:57:58 -0800

    profile reducer and get current profile

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
+import { clearCurrentProfile } from './actions/profileActions';
 
 import './App.css';
 
@@ -27,9 +29,12 @@ if (localStorage.jwtToken) {
   // Check for expired token
   const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
+    
     // Logout user
     store.dispatch(logoutUser());
     // TODO: Clear current Profile
+    store.dispatch(clearCurrentProfile());
+
 
     // Redirect to login
     window.location.href = '/login';
@@ -47,6 +52,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
