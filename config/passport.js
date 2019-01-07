e85cc96d17c2b1f1d84fed3162a55a9445ba64c0
Herman Wu <herman@cloudistics.com>
2019-01-06 22:23:47 -0800

    fix config

--- config/passport.js
+++ config/passport.js
@@ -12,13 +12,13 @@ module.exports = passport => {
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
       User.findById(jwt_payload.id)
-      .then(user => {
-        if (user) {
-          return done(null, user);
-        }
-        return done(null, false);
-      })
-      .catch(error => console.log(error));
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch(err => console.log(err));
     })
   );
-}
+};
