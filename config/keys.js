8648114cbd8ca09393b09388fb409cb7fe0ae310
Herman Wu <herman@cloudistics.com>
2019-01-06 21:54:46 -0800

    set config

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://zwu369:zwu369@ds135714.mlab.com:35714/firstdb',
-  secretOrKey: 'secret'
-};
+if (process.env.MODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
